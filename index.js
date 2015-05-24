89e4cbedc25e1ee06f61ae50a3200811ae2d926d
Sergey Romanov <xxsmotur@gmail.com>
2015-05-25 04:11:41 +0500

    Fixed triple equality

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ exports.parse = function (str) {
 		return {};
 	}
 
-	if (str[0] == '&') {
+	if (str[0] === '&') {
 		str = str.substr(1, str.length);
 	}
-	
+
 	str = str.trim().replace(/^(\?|#)/, '');
 
 	if (!str) {
