cb8377d322df256909ef5dffc561fd6faba2a1eb
Brandon Pugh <bp@brandonpugh.com>
2015-12-03 17:24:39 -0600

    Make tests compatible with latest version of AVA
    Tests were throwing the 't.end is not supported in this context.' with
    latest version of AVA so I removed all the t.end() calls since it seems
    to no longer be neccessary.

--- test/stringify.js
+++ test/stringify.js
@@ -4,37 +4,30 @@ import fn from '../';
 test('stringify', t => {
 	t.same(fn.stringify({foo: 'bar'}), 'foo=bar');
 	t.same(fn.stringify({foo: 'bar', bar: 'baz'}), 'bar=baz&foo=bar');
-	t.end();
 });
 
 test('different types', t => {
 	t.same(fn.stringify(), '');
 	t.same(fn.stringify(0), '');
-	t.end();
 });
 
 test('URI encode', t => {
 	t.same(fn.stringify({'foo bar': 'baz faz'}), 'foo%20bar=baz%20faz');
 	t.same(fn.stringify({'foo bar': 'baz\'faz'}), 'foo%20bar=baz%27faz');
-	t.end();
 });
 
 test('handle array value', t => {
 	t.same(fn.stringify({abc: 'abc', foo: ['bar', 'baz']}), 'abc=abc&foo=bar&foo=baz');
-	t.end();
 });
 
 test('handle empty array value', t => {
 	t.same(fn.stringify({abc: 'abc', foo: []}), 'abc=abc');
-	t.end();
 });
 
 test('should not encode undefined values', t => {
 	t.same(fn.stringify({abc: undefined, foo: 'baz'}), 'foo=baz');
-	t.end();
 });
 
 test('should encode null values as just a key', t => {
 	t.same(fn.stringify({xyz: null, abc: null, foo: 'baz'}), 'abc&foo=baz&xyz');
-	t.end();
 });
