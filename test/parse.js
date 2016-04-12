5345100294b26ce82e3cdcbf9b1d8c679784643b
Sam Verschueren <sam.verschueren@gmail.com>
2016-04-12 15:56:14 +0200

    :arrow_up: ava@0.14.0 (#57)

--- test/parse.js
+++ test/parse.js
@@ -1,70 +1,58 @@
 import test from 'ava';
 import fn from '../';
 
-// https://github.com/sindresorhus/query-string/pull/48#issuecomment-198242935
-function tsame(t, actual, expected) {
-	Object.keys(expected).forEach(key => {
-		if (Array.isArray(expected[key])) {
-			t.same(actual[key], expected[key]);
-		} else {
-			t.is(actual[key], expected[key]);
-		}
-	});
-}
-
 test('query strings starting with a `?`', t => {
-	tsame(t, fn.parse('?foo=bar'), {foo: 'bar'});
+	t.deepEqual(fn.parse('?foo=bar'), {foo: 'bar'});
 });
 
 test('query strings starting with a `#`', t => {
-	tsame(t, fn.parse('#foo=bar'), {foo: 'bar'});
+	t.deepEqual(fn.parse('#foo=bar'), {foo: 'bar'});
 });
 
 test('query strings starting with a `&`', t => {
-	tsame(t, fn.parse('&foo=bar&foo=baz'), {foo: ['bar', 'baz']});
+	t.deepEqual(fn.parse('&foo=bar&foo=baz'), {foo: ['bar', 'baz']});
 });
 
 test('parse a query string', t => {
-	tsame(t, fn.parse('foo=bar'), {foo: 'bar'});
+	t.deepEqual(fn.parse('foo=bar'), {foo: 'bar'});
 });
 
 test('parse multiple query string', t => {
-	tsame(t, fn.parse('foo=bar&key=val'), {foo: 'bar', key: 'val'});
+	t.deepEqual(fn.parse('foo=bar&key=val'), {foo: 'bar', key: 'val'});
 });
 
 test('parse query string without a value', t => {
-	tsame(t, fn.parse('foo'), {foo: null});
-	tsame(t, fn.parse('foo&key'), {foo: null, key: null});
-	tsame(t, fn.parse('foo=bar&key'), {foo: 'bar', key: null});
-	tsame(t, fn.parse('a&a'), {a: [null, null]});
-	tsame(t, fn.parse('a=&a'), {a: ['', null]});
+	t.deepEqual(fn.parse('foo'), {foo: null});
+	t.deepEqual(fn.parse('foo&key'), {foo: null, key: null});
+	t.deepEqual(fn.parse('foo=bar&key'), {foo: 'bar', key: null});
+	t.deepEqual(fn.parse('a&a'), {a: [null, null]});
+	t.deepEqual(fn.parse('a=&a'), {a: ['', null]});
 });
 
 test('return empty object if no qss can be found', t => {
-	tsame(t, fn.parse('?'), {});
-	tsame(t, fn.parse('&'), {});
-	tsame(t, fn.parse('#'), {});
-	tsame(t, fn.parse(' '), {});
+	t.deepEqual(fn.parse('?'), {});
+	t.deepEqual(fn.parse('&'), {});
+	t.deepEqual(fn.parse('#'), {});
+	t.deepEqual(fn.parse(' '), {});
 });
 
 test('handle `+` correctly', t => {
-	tsame(t, fn.parse('foo+faz=bar+baz++'), {'foo faz': 'bar baz  '});
+	t.deepEqual(fn.parse('foo+faz=bar+baz++'), {'foo faz': 'bar baz  '});
 });
 
 test('handle multiple of the same key', t => {
-	tsame(t, fn.parse('foo=bar&foo=baz'), {foo: ['bar', 'baz']});
+	t.deepEqual(fn.parse('foo=bar&foo=baz'), {foo: ['bar', 'baz']});
 });
 
 test('query strings params including embedded `=`', t => {
-	tsame(t, fn.parse('?param=http%3A%2F%2Fsomeurl%3Fid%3D2837'), {param: 'http://someurl?id=2837'});
+	t.deepEqual(fn.parse('?param=http%3A%2F%2Fsomeurl%3Fid%3D2837'), {param: 'http://someurl?id=2837'});
 });
 
 test('query strings params including raw `=`', t => {
-	tsame(t, fn.parse('?param=http://someurl?id=2837'), {param: 'http://someurl?id=2837'});
+	t.deepEqual(fn.parse('?param=http://someurl?id=2837'), {param: 'http://someurl?id=2837'});
 });
 
 test('object properties', t => {
-	t.notOk(fn.parse().prototype);
-	tsame(t, fn.parse('hasOwnProperty=foo'), {hasOwnProperty: 'foo'});
-	tsame(t, fn.parse('__proto__=bar'), {__proto__: 'bar'});
+	t.falsy(fn.parse().prototype);
+	t.deepEqual(fn.parse('hasOwnProperty=foo'), {hasOwnProperty: 'foo'});
 });
--- test/stringify.js
+++ test/stringify.js
@@ -2,54 +2,54 @@ import test from 'ava';
 import fn from '../';
 
 test('stringify', t => {
-	t.same(fn.stringify({foo: 'bar'}), 'foo=bar');
-	t.same(fn.stringify({foo: 'bar', bar: 'baz'}), 'bar=baz&foo=bar');
+	t.deepEqual(fn.stringify({foo: 'bar'}), 'foo=bar');
+	t.deepEqual(fn.stringify({foo: 'bar', bar: 'baz'}), 'bar=baz&foo=bar');
 });
 
 test('different types', t => {
-	t.same(fn.stringify(), '');
-	t.same(fn.stringify(0), '');
+	t.deepEqual(fn.stringify(), '');
+	t.deepEqual(fn.stringify(0), '');
 });
 
 test('URI encode', t => {
-	t.same(fn.stringify({'foo bar': 'baz faz'}), 'foo%20bar=baz%20faz');
-	t.same(fn.stringify({'foo bar': 'baz\'faz'}), 'foo%20bar=baz%27faz');
+	t.deepEqual(fn.stringify({'foo bar': 'baz faz'}), 'foo%20bar=baz%20faz');
+	t.deepEqual(fn.stringify({'foo bar': 'baz\'faz'}), 'foo%20bar=baz%27faz');
 });
 
 test('handle array value', t => {
-	t.same(fn.stringify({abc: 'abc', foo: ['bar', 'baz']}), 'abc=abc&foo=bar&foo=baz');
+	t.deepEqual(fn.stringify({abc: 'abc', foo: ['bar', 'baz']}), 'abc=abc&foo=bar&foo=baz');
 });
 
 test('handle empty array value', t => {
-	t.same(fn.stringify({abc: 'abc', foo: []}), 'abc=abc');
+	t.deepEqual(fn.stringify({abc: 'abc', foo: []}), 'abc=abc');
 });
 
 test('should not encode undefined values', t => {
-	t.same(fn.stringify({abc: undefined, foo: 'baz'}), 'foo=baz');
+	t.deepEqual(fn.stringify({abc: undefined, foo: 'baz'}), 'foo=baz');
 });
 
 test('should encode null values as just a key', t => {
-	t.same(fn.stringify({xyz: null, abc: null, foo: 'baz'}), 'abc&foo=baz&xyz');
+	t.deepEqual(fn.stringify({xyz: null, abc: null, foo: 'baz'}), 'abc&foo=baz&xyz');
 });
 
 test('handle null values in array', t => {
-	t.same(fn.stringify({foo: null, bar: [null, 'baz']}), 'bar=baz&bar&foo');
+	t.deepEqual(fn.stringify({foo: null, bar: [null, 'baz']}), 'bar=baz&bar&foo');
 });
 
 test('handle undefined values in array', t => {
-	t.same(fn.stringify({foo: null, bar: [undefined, 'baz']}), 'bar=baz&foo');
+	t.deepEqual(fn.stringify({foo: null, bar: [undefined, 'baz']}), 'bar=baz&foo');
 });
 
 test('handle undefined and null values in array', t => {
-	t.same(fn.stringify({foo: null, bar: [undefined, null, 'baz']}), 'bar=baz&bar&foo');
+	t.deepEqual(fn.stringify({foo: null, bar: [undefined, null, 'baz']}), 'bar=baz&bar&foo');
 });
 
 test('strict encoding', t => {
-	t.same(fn.stringify({foo: '\'bar\''}), 'foo=%27bar%27');
-	t.same(fn.stringify({foo: ['\'bar\'', '!baz']}), 'foo=%21baz&foo=%27bar%27');
+	t.deepEqual(fn.stringify({foo: '\'bar\''}), 'foo=%27bar%27');
+	t.deepEqual(fn.stringify({foo: ['\'bar\'', '!baz']}), 'foo=%21baz&foo=%27bar%27');
 });
 
 test('loose encoding', t => {
-	t.same(fn.stringify({foo: '\'bar\''}, {strict: false}), 'foo=\'bar\'');
-	t.same(fn.stringify({foo: ['\'bar\'', '!baz']}, {strict: false}), 'foo=!baz&foo=\'bar\'');
+	t.deepEqual(fn.stringify({foo: '\'bar\''}, {strict: false}), 'foo=\'bar\'');
+	t.deepEqual(fn.stringify({foo: ['\'bar\'', '!baz']}, {strict: false}), 'foo=!baz&foo=\'bar\'');
 });
