c0d81b3659e56631956e2879a22bd015809cb8db
Romain Pellerin <romain.pellerin@doctolib.com>
2018-05-04 11:48:37 +0200

    Improve ES6 syntax

--- index.js
+++ index.js
@@ -1,222 +1,227 @@
-'use strict';
-const strictUriEncode = require('strict-uri-encode');
-const decodeComponent = require('decode-uri-component');
+'use strict'
+const strictUriEncode = require('strict-uri-encode')
+const decodeComponent = require('decode-uri-component')
 
-function encoderForArrayFormat(options) {
+const encoderForArrayFormat = options => {
 	switch (options.arrayFormat) {
 		case 'index':
 			return (key, value, index) => {
-				return value === null ? [
-					encode(key, options),
-					'[',
-					index,
-					']'
-				].join('') : [
-					encode(key, options),
-					'[',
-					encode(index, options),
-					']=',
-					encode(value, options)
-				].join('');
-			};
+				return value === null
+					? [encode(key, options), '[', index, ']'].join('')
+					: [
+							encode(key, options),
+							'[',
+							encode(index, options),
+							']=',
+							encode(value, options)
+					  ].join('')
+			}
 		case 'bracket':
 			return (key, value) => {
-				return value === null ? [encode(key, options), '[]'].join('') : [
-					encode(key, options),
-					'[]=',
-					encode(value, options)
-				].join('');
-			};
+				return value === null
+					? [encode(key, options), '[]'].join('')
+					: [encode(key, options), '[]=', encode(value, options)].join('')
+			}
 		default:
 			return (key, value) => {
-				return value === null ? encode(key, options) : [
-					encode(key, options),
-					'=',
-					encode(value, options)
-				].join('');
-			};
+				return value === null
+					? encode(key, options)
+					: [encode(key, options), '=', encode(value, options)].join('')
+			}
 	}
 }
 
-function parserForArrayFormat(options) {
-	let result;
+const parserForArrayFormat = options => {
+	let result
 
 	switch (options.arrayFormat) {
 		case 'index':
 			return (key, value, accumulator) => {
-				result = /\[(\d*)\]$/.exec(key);
+				result = /\[(\d*)\]$/.exec(key)
 
-				key = key.replace(/\[\d*\]$/, '');
+				key = key.replace(/\[\d*\]$/, '')
 
 				if (!result) {
-					accumulator[key] = value;
-					return;
+					accumulator[key] = value
+					return
 				}
 
 				if (accumulator[key] === undefined) {
-					accumulator[key] = {};
+					accumulator[key] = {}
 				}
 
-				accumulator[key][result[1]] = value;
-			};
+				accumulator[key][result[1]] = value
+			}
 		case 'bracket':
 			return (key, value, accumulator) => {
-				result = /(\[\])$/.exec(key);
-				key = key.replace(/\[\]$/, '');
+				result = /(\[\])$/.exec(key)
+				key = key.replace(/\[\]$/, '')
 
 				if (!result) {
-					accumulator[key] = value;
-					return;
+					accumulator[key] = value
+					return
 				}
 
 				if (accumulator[key] === undefined) {
-					accumulator[key] = [value];
-					return;
+					accumulator[key] = [value]
+					return
 				}
 
-				accumulator[key] = [].concat(accumulator[key], value);
-			};
+				accumulator[key] = [].concat(accumulator[key], value)
+			}
 		default:
 			return (key, value, accumulator) => {
 				if (accumulator[key] === undefined) {
-					accumulator[key] = value;
-					return;
+					accumulator[key] = value
+					return
 				}
 
-				accumulator[key] = [].concat(accumulator[key], value);
-			};
+				accumulator[key] = [].concat(accumulator[key], value)
+			}
 	}
 }
 
-function encode(value, options) {
+const encode = (value, options) => {
 	if (options.encode) {
-		return options.strict ? strictUriEncode(value) : encodeURIComponent(value);
+		return options.strict ? strictUriEncode(value) : encodeURIComponent(value)
 	}
 
-	return value;
+	return value
 }
 
-function decode(value, options) {
+const decode = (value, options) => {
 	if (options.decode) {
-		return decodeComponent(value);
+		return decodeComponent(value)
 	}
 
-	return value;
+	return value
 }
 
-function keysSorter(input) {
+const keysSorter = input => {
 	if (Array.isArray(input)) {
-		return input.sort();
+		return input.sort()
 	}
 
 	if (typeof input === 'object') {
 		return keysSorter(Object.keys(input))
 			.sort((a, b) => Number(a) - Number(b))
-			.map(key => input[key]);
+			.map(key => input[key])
 	}
 
-	return input;
+	return input
 }
 
-function extract(input) {
-	const queryStart = input.indexOf('?');
+const extract = input => {
+	const queryStart = input.indexOf('?')
 	if (queryStart === -1) {
-		return '';
+		return ''
 	}
-	return input.slice(queryStart + 1);
+	return input.slice(queryStart + 1)
 }
 
-function parse(input, options) {
-	options = Object.assign({decode: true, arrayFormat: 'none'}, options);
+const parse = (input, options) => {
+	options = Object.assign({ decode: true, arrayFormat: 'none' }, options)
 
-	const formatter = parserForArrayFormat(options);
+	const formatter = parserForArrayFormat(options)
 
 	// Create an object with no prototype
-	const ret = Object.create(null);
+	const ret = Object.create(null)
 
 	if (typeof input !== 'string') {
-		return ret;
+		return ret
 	}
 
-	input = input.trim().replace(/^[?#&]/, '');
+	input = input.trim().replace(/^[?#&]/, '')
 
 	if (!input) {
-		return ret;
+		return ret
 	}
 
 	for (const param of input.split('&')) {
-		let [key, value] = param.replace(/\+/g, ' ').split('=');
+		let [key, value] = param.replace(/\+/g, ' ').split('=')
 
 		// Missing `=` should be `null`:
 		// http://w3.org/TR/2012/WD-url-20120524/#collect-url-parameters
-		value = value === undefined ? null : decode(value, options);
+		value = value === undefined ? null : decode(value, options)
 
-		formatter(decode(key, options), value, ret);
+		formatter(decode(key, options), value, ret)
 	}
 
-	return Object.keys(ret).sort().reduce((result, key) => {
-		const value = ret[key];
-		if (Boolean(value) && typeof value === 'object' && !Array.isArray(value)) {
-			// Sort object keys, not values
-			result[key] = keysSorter(value);
-		} else {
-			result[key] = value;
-		}
+	return Object.keys(ret)
+		.sort()
+		.reduce((result, key) => {
+			const value = ret[key]
+			if (
+				Boolean(value) &&
+				typeof value === 'object' &&
+				!Array.isArray(value)
+			) {
+				// Sort object keys, not values
+				result[key] = keysSorter(value)
+			} else {
+				result[key] = value
+			}
 
-		return result;
-	}, Object.create(null));
+			return result
+		}, Object.create(null))
 }
 
-exports.extract = extract;
-exports.parse = parse;
+exports.extract = extract
+exports.parse = parse
 
 exports.stringify = (obj, options) => {
 	const defaults = {
 		encode: true,
 		strict: true,
 		arrayFormat: 'none'
-	};
-
-	options = Object.assign(defaults, options);
-
-	if (options.sort === false) {
-		options.sort = () => {};
 	}
 
-	const formatter = encoderForArrayFormat(options);
+	options = Object.assign(defaults, options)
 
-	return obj ? Object.keys(obj).sort(options.sort).map(key => {
-		const value = obj[key];
+	if (options.sort === false) {
+		options.sort = () => {}
+	}
 
-		if (value === undefined) {
-			return '';
-		}
+	const formatter = encoderForArrayFormat(options)
 
-		if (value === null) {
-			return encode(key, options);
-		}
+	return obj
+		? Object.keys(obj)
+				.sort(options.sort)
+				.map(key => {
+					const value = obj[key]
 
-		if (Array.isArray(value)) {
-			const result = [];
+					if (value === undefined) {
+						return ''
+					}
 
-			for (const value2 of value.slice()) {
-				if (value2 === undefined) {
-					continue;
-				}
+					if (value === null) {
+						return encode(key, options)
+					}
 
-				result.push(formatter(key, value2, result.length));
-			}
+					if (Array.isArray(value)) {
+						const result = []
 
-			return result.join('&');
-		}
+						for (const value2 of value.slice()) {
+							if (value2 === undefined) {
+								continue
+							}
 
-		return encode(key, options) + '=' + encode(value, options);
-	}).filter(x => x.length > 0).join('&') : '';
-};
+							result.push(formatter(key, value2, result.length))
+						}
+
+						return result.join('&')
+					}
+
+					return encode(key, options) + '=' + encode(value, options)
+				})
+				.filter(x => x.length > 0)
+				.join('&')
+		: ''
+}
 
 exports.parseUrl = (input, options) => {
 	return {
 		url: input.split('?')[0] || '',
 		query: parse(extract(input), options)
-	};
-};
+	}
+}
