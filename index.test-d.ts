2c2e77a3beefc70432630ed22428640342f0385f
Dimitri Benin <BendingBender@users.noreply.github.com>
2019-03-03 11:06:32 +0000

    Add TypeScript definition (#168)

---
+++ index.test-d.ts
@@ -0,0 +1,68 @@
+import {expectType} from 'tsd-check';
+import * as queryString from '.';
+
+// Stringify
+expectType<string>(
+	queryString.stringify({
+		str: 'bar',
+		strArray: ['baz'],
+		num: 123,
+		numArray: [456],
+		bool: true,
+		boolArray: [false]
+	})
+);
+
+expectType<string>(queryString.stringify({foo: 'bar'}, {strict: false}));
+expectType<string>(queryString.stringify({foo: 'bar'}, {encode: false}));
+expectType<string>(
+	queryString.stringify({foo: 'bar'}, {arrayFormat: 'bracket'})
+);
+expectType<string>(queryString.stringify({foo: 'bar'}, {arrayFormat: 'index'}));
+expectType<string>(queryString.stringify({foo: 'bar'}, {arrayFormat: 'none'}));
+expectType<string>(queryString.stringify({foo: 'bar'}, {sort: false}));
+const order = ['c', 'a', 'b'];
+expectType<string>(
+	queryString.stringify(
+		{foo: 'bar'},
+		{
+			sort: (itemLeft, itemRight) =>
+				order.indexOf(itemLeft) - order.indexOf(itemRight)
+		}
+	)
+);
+
+// Parse
+expectType<queryString.ParsedQuery>(queryString.parse('?foo=bar'));
+
+expectType<queryString.ParsedQuery>(
+	queryString.parse('?foo=bar', {decode: false})
+);
+expectType<queryString.ParsedQuery>(
+	queryString.parse('?foo=bar', {arrayFormat: 'bracket'})
+);
+expectType<queryString.ParsedQuery>(
+	queryString.parse('?foo=bar', {arrayFormat: 'index'})
+);
+expectType<queryString.ParsedQuery>(
+	queryString.parse('?foo=bar', {arrayFormat: 'none'})
+);
+
+// Parse URL
+expectType<queryString.ParsedUrl>(queryString.parseUrl('?foo=bar'));
+
+expectType<queryString.ParsedUrl>(
+	queryString.parseUrl('?foo=bar', {decode: false})
+);
+expectType<queryString.ParsedUrl>(
+	queryString.parseUrl('?foo=bar', {arrayFormat: 'bracket'})
+);
+expectType<queryString.ParsedUrl>(
+	queryString.parseUrl('?foo=bar', {arrayFormat: 'index'})
+);
+expectType<queryString.ParsedUrl>(
+	queryString.parseUrl('?foo=bar', {arrayFormat: 'none'})
+);
+
+// Extract
+expectType<string>(queryString.extract('http://foo.bar/?abc=def&hij=klm'));
