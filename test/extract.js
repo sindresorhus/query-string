64a30da7bd26a94645edfe47c2d7eb54b98d0684
ChrisCindy <yxy0919@foxmail.com>
2019-04-28 16:51:48 +0800

    Support hash in `.extract()` (#174)

--- test/extract.js
+++ test/extract.js
@@ -5,10 +5,13 @@ test('extracts query string from url', t => {
 	t.is(queryString.extract('https://foo.bar/?abc=def&hij=klm'), 'abc=def&hij=klm');
 	t.is(queryString.extract('https://foo.bar/?'), '');
 	t.is(queryString.extract('https://foo.bar/?regex=ab?c'), 'regex=ab?c');
+	t.is(queryString.extract('https://foo.bar#top?foo=bar'), '');
+	t.is(queryString.extract('https://foo.bar?foo=bar#top'), 'foo=bar');
 });
 
 test('handles strings not containing query string', t => {
 	t.is(queryString.extract('https://foo.bar'), '');
+	t.is(queryString.extract('https://foo.bar#top'), '');
 	t.is(queryString.extract(''), '');
 });
 
