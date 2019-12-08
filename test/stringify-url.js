c424ee5f4adb31c5b586849e7ead7dd0a5713e46
komcal <kom.sujautra@gmail.com>
2019-12-08 12:35:41 +0700

    update test result of parseUrl

--- test/stringify-url.js
+++ test/stringify-url.js
@@ -15,21 +15,24 @@ test('stringify URL with a query string', t => {
 	t.deepEqual(queryString.stringifyUrl({url: 'https://foo.bar?', query: {foo: 'bar'}}), 'https://foo.bar?foo=bar');
 	t.deepEqual(queryString.stringifyUrl({url: 'https://foo.bar/#top', query: {foo: 'bar'}}), 'https://foo.bar/?foo=bar#top');
 	t.deepEqual(queryString.stringifyUrl({url: 'https://foo.bar', query: {foo: 'bar', a: 'b'}}), 'https://foo.bar?a=b&foo=bar');
-	t.deepEqual(queryString.stringifyUrl({url: 'https://foo.bar?', query: {foo: ['bar', 'baz']}}), 'https://foo.bar?foo=bar&foo=baz');
+	t.deepEqual(queryString.stringifyUrl({url: 'https://foo.bar?a=b', query: {foo: ['bar', 'baz']}}), 'https://foo.bar?a=b&foo=bar&foo=baz');
 	t.deepEqual(queryString.stringifyUrl({url: 'https://foo.bar?foo=baz', query: {foo: 'bar'}}), 'https://foo.bar?foo=bar');
 });
 
 test('stringify URL from the result of `parseUrl` without query string', t => {
-	const parsedUrl = queryString.parseUrl('https://foo.bar#top');
-	t.deepEqual(queryString.stringifyUrl(parsedUrl), 'https://foo.bar');
+	const url = 'https://foo.bar';
+	const parsedUrl = queryString.parseUrl(url);
+	t.deepEqual(queryString.stringifyUrl(parsedUrl), url);
 });
 
 test('stringify URL from the result of `parseUrl` with query string', t => {
-	const parsedUrl = queryString.parseUrl('https://foo.bar?foo=bar&foo=baz#top');
-	t.deepEqual(queryString.stringifyUrl(parsedUrl), 'https://foo.bar?foo=bar&foo=baz');
+	const url = 'https://foo.bar?foo=bar&foo=baz';
+	const parsedUrl = queryString.parseUrl(url);
+	t.deepEqual(queryString.stringifyUrl(parsedUrl), url);
 });
 
 test('stringify URL from the result of `parseUrl` with query string that contains `=`', t => {
-	const parsedUrl = queryString.parseUrl('https://foo.bar?foo=bar=&foo=baz=');
-	t.deepEqual(queryString.stringifyUrl(parsedUrl, {encode: false}), 'https://foo.bar?foo=bar=&foo=baz=');
+	const url = 'https://foo.bar?foo=bar=&foo=baz=';
+	const parsedUrl = queryString.parseUrl(url);
+	t.deepEqual(queryString.stringifyUrl(parsedUrl, {encode: false}), url);
 });
