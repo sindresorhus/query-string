b7a2c407818aa9a0df1d88469acbee012ec99c4c
Ethan Setnik <esetnik@gmail.com>
2020-10-02 08:04:42 -0400

    add undefined as well

--- index.d.ts
+++ index.d.ts
@@ -348,10 +348,10 @@ export interface StringifyOptions {
 	readonly skipEmptyString?: boolean;
 }
 
-export type Stringifiable = string | boolean | number | null;
+export type Stringifiable = string | boolean | number | null | undefined;
 export type StringifiableRecord = Record<
 	string,
-	Stringifiable | Stringifiable[] | null | undefined
+	Stringifiable | Stringifiable[]
 >;
 
 /**
