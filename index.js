735dcec29f6d8b2c4e14c61d631a2dd1b3811e1a
Matt Sandler <mishugana@gmail.com>
2020-10-16 10:40:03 -0400

    Using Array#includes method and not splitting the string into an array

--- index.js
+++ index.js
@@ -122,8 +122,8 @@ function parserForArrayFormat(options) {
 		case 'comma':
 		case 'separator':
 			return (key, value, accumulator) => {
-				const isArray = typeof value === 'string' && value.split('').indexOf(options.arrayFormatSeparator) > -1;
-				const isEncodedArray = (typeof value === 'string' && !isArray && decode(value, options).split('').indexOf(options.arrayFormatSeparator) > -1);
+				const isArray = typeof value === 'string' && value.includes(options.arrayFormatSeparator);
+				const isEncodedArray = (typeof value === 'string' && !isArray && decode(value, options).includes(options.arrayFormatSeparator));
 				value = isEncodedArray ? decode(value, options) : value;
 				const newValue = isArray || isEncodedArray ? value.split(options.arrayFormatSeparator).map(item => decode(item, options)) : value === null ? value : decode(value, options);
 				accumulator[key] = newValue;
