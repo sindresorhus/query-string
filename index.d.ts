76f8fc5990932fe55beab5d27340731081c3c8b4
Sindre Sorhus <sindresorhus@gmail.com>
2022-12-13 10:57:58 +0100

    Fix TypeScript compatibility
    Fixes #358

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,9 @@
-export * as default from './base.js';
+/// export * as default from './base.js';
+
+// Workaround for TS missing feature.
+import * as queryString from './base.js';
+
+export default queryString;
 
 export {
 	type ParseOptions,
